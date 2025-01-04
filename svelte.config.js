026d07cd72054c35bbef3799dc1a16c075e3fbfb
yttasdfghjk <yttasdfghjk@proton.me>
2025-01-04 13:20:08 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
- 
+//import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {imdwqdport('@sveltejs/kit').Config} */
 const config = {
+	
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
