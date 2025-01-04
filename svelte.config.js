b213bab0104ed7d0940eb2abfa3d68b3931e8a93
yttasdfghjk <alparslan-labs@proton.me>
2025-01-04 13:33:54 +0100

    conf

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 //import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {imdwqdport('@sveltejs/kit').Config} */
 const config = {
-	
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production'?'/alparslan-labs.github.io' :'',
+
+		}
 	},
 };
  
-config.paths = { base: process.argv.includes('dev') ? '' : "/alparslan-labs" }
+//config.paths = { base: process.argv.includes('dev') ? '' : "/alparslan-labs" }
  
 export default config;
 
