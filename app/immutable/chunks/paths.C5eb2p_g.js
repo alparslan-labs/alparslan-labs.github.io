dca1c8b493adc09dd5c9702f107ef9dc3027252b
yttasdfghjk <alparslan-labs@proton.me>
2025-01-06 06:10:16 +0100

    Updates

---
+++ app/immutable/chunks/paths.C5eb2p_g.js
@@ -0,0 +1,1 @@
+var s;const o=((s=globalThis.__sveltekit_oofrmc)==null?void 0:s.base)??"/alparslan-labs.github.io";var a;const t=((a=globalThis.__sveltekit_oofrmc)==null?void 0:a.assets)??o;export{t as a,o as b};
