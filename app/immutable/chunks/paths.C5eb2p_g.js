c82a5704689048f4186bf16b90e80b5b75428836
yttasdfghjk <alparslan-labs@proton.me>
2025-01-06 08:30:46 +0100

    Updates

--- app/immutable/chunks/paths.C5eb2p_g.js
+++
@@ -1,1 +0,0 @@
-var s;const o=((s=globalThis.__sveltekit_oofrmc)==null?void 0:s.base)??"/alparslan-labs.github.io";var a;const t=((a=globalThis.__sveltekit_oofrmc)==null?void 0:a.assets)??o;export{t as a,o as b};
---
+++ app/immutable/entry/start.D_LSPd9V.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bnc4dg3s.js";export{t as start};
